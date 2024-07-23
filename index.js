2bb1408dc95f84e5d4aa5d53c817e508a20ff99a
Wilbur W <wilburwei.2008@gmail.com>
2024-07-22 20:17:32 -0400

    Finish side navigation animation

---
+++ index.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+  $("#mbar").click(function () {
+    $("#mshadow").css("display","block")
+    $("#menu").css("display", "block")
+    $("#menulinks").css("display", "flex")
+    $("#mx").css("display", "block")
+    
+    $("#mshadow").animate({
+      opacity:0.5
+    }, 500)
+    $("#menu").animate({
+      width:300,
+      opacity:1,
+    }, 500, function () {
+      $("#menulinks").animate({
+        opacity: 1
+      }, 300)
+    })
+    $("#mx").animate({
+      opacity:1,
+    }, 500)
+  })
+  $("#mx").click(function() {
+    $("#menulinks").animate({
+      opacity: 0
+    }, 100, function() {
+      $("#menu").animate({
+        width: 0,
+        opacity: 0,
+      }, 400)
+      $("#mx").animate({
+        opacity: 0,
+      }, 400, function() {
+        $("#mshadow").css("display", "none")
+        $("#menu").css("display", "none")
+        $("#menulinks").css("display", "none")
+      })
+    })
+    $("#mshadow").animate({
+      opacity: 0
+    }, 500)
+
+
+  })
+}
